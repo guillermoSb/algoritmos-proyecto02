5c634d2d3ab87429f24b1481c3c3480898b4c6fb
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-17 08:39:59 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+    UNIVERSIDAD DEL VALLE DE GUATEMALA
+    ALGORITMOS Y ESTRUCTURAS DE DATOS
+    PROYECTO #2 - Motor de Recomendaciones
+"""
+
+def init():
+    print("Iniciando motor de recomendaciones")
+
+
+if __name__ == '__main__':
+    init()
+
