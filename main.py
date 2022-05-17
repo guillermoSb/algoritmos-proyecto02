9299ce693fafb9791d168178c9b1da9ae7194b5c
Guillermo Santos <guillermo.santosba@gmail.com>
2022-05-17 09:04:22 -0600

    Implemented NEO4J Connection

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-"""
-    UNIVERSIDAD DEL VALLE DE GUATEMALA
-    ALGORITMOS Y ESTRUCTURAS DE DATOS
-    PROYECTO #2 - Motor de Recomendaciones
-"""
+# Universidad del Valle de Guatemala
+# Algoritmos y Estructuras de Datos
+# Proyecto #2 - Motor de Recomendaciones
+
+from engine import RecommendationEngine
+
 
 def init():
     print("Iniciando motor de recomendaciones")
+    engine = RecommendationEngine("neo4j+s://1693e124.databases.neo4j.io", "neo4j",
+                                  "kwRfn1Ak-qqe7Jxybt8SwplT5--cP1cfEcIuo5tJlnM")
+    engine.print_greeting("guille")
 
 
 if __name__ == '__main__':
